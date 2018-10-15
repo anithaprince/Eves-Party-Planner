4fd1f632194803d17d3f5627fce9cf68e0e82086
Anitha Prince <anithaprince@Princes-MacBook-Pro.local>
2018-10-15 01:26:20 -0400

    packages generated using seed and views route done

--- controllers/party.js
+++ controllers/party.js
@@ -8,10 +8,21 @@ const bcrypt = require('bcrypt')
 //              DATABASE
 // =======================================
 const Party = require('../models/party.js');
+const partySeeds = require ('../models/seed.js');
 
 // =======================================
 //              ROUTES
 // =======================================
 
+router.get ('/seed' , (req, res) => {
+  Party.insertMany(partySeeds,(err, data) =>{
+    if ( err ) { console.log( err ); }
+    console.log( 'SEED: NEW PRODUCTS CREATED!' );
+    res.redirect('/');
+  })
+});
 
+router.get('/packages', (req, res)=>{
+    res.render('packages.ejs');
+})
 module.exports = router;
--- models/party.js
+++ models/party.js
@@ -4,11 +4,13 @@ const Schema = mongoose.Schema
 const partySchema = Schema({
   title: { type: String, required: true },
   description: {type: String},
+  highlights: {type: String},
   img: {type: String},
   price:{
           type: Number,
           min :[ 0, 'Price can\'t be less than 0. This ain\'t no charity!']
   },
+  note: {type: String}
 })
 
 const Party = mongoose.model('Party', partySchema)
