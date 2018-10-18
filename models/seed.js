f19649db2df400c43ff458770508a403372f301b
Anitha Prince <anithaprince@Princes-MacBook-Pro.local>
2018-10-18 01:26:52 -0400

    more seed date added

--- models/seed.js
+++ models/seed.js
@@ -82,5 +82,107 @@ module.exports = [
               "*Additonal kids are $12.00 each",
               "*For additonal cost of $250.00 add a photographer and a 30 min photo shoot for the birthday kid prior to the party."
             ]
+        },
+        {
+            title:'Platinum Package',
+            description:'This package offers fun activities for children of all ages. This is a great addition to our DJ package or as an independent package.',
+            highlights:[
+              "Tier #1 – Spin Art, Sand Art, Candy Art",
+              "Tier #2 – Face Painting, Balloon Sculpting, Temporary Tattoo’s",
+              "LED Party Lights: Professionally installed LED lights with 360° special effects",
+              "LED Party Lights: Professionally installed LED lights with 360° special effects",
+              "End the evening on a sweet note - with a presentation of the ultimate Sweet Treats Table!",
+              "Roll out the Red Carpet! Your guests are greeted to the Manor with a long, plush red carpet, and treated to a sparkling beverage with white glove tray service. (non-alcoholic.)",
+              "Live viewing: View your event live on our over-sized screen",
+              "Theme Invites",
+              "Up to 3 Centerpieces",
+              "Place Settings (Disposable, plates, cups, napkins and cups)",
+              "Party Favors",
+              "3 Bouquet of Balloons",
+              "1 Table and 8 Chairs",
+              "Plastic Tablecloth "
+            ],
+            img: 'images/package6.jpg',
+            price: 5000,
+            note:[
+              "This package offers fun activities for children of all ages. This is a great addition to our DJ package or as an independent package.",
+            ]
+        },
+        {
+            title:'Sweet 15/16',
+            description:'Be a Princess for a Night at the Princess Manor. Let us create the Sweet 16 party of your dreams! From impressive gourmet food selections to elegant décor, we have all of your needs covered.',
+            highlights:[
+              " Buffet-style food service with staff attendants • Beverages: soft drinks and International coffee & tea station",
+              "Tier #2 – Face Painting, Balloon Sculpting, Temporary Tattoo’s",
+              "LED Party Lights: Professionally installed LED lights with 360° special effects",
+              "LED Party Lights: Professionally installed LED lights with 360° special effects",
+              "End the evening on a sweet note - with a presentation of the ultimate Sweet Treats Table!",
+              "Roll out the Red Carpet! Your guests are greeted to the Manor with a long, plush red carpet, and treated to a sparkling beverage with white glove tray service. (non-alcoholic.)",
+              "Live viewing: View your event live on our over-sized screen",
+              "Theme Invites",
+              "Up to 3 Centerpieces",
+              "Place Settings (Disposable, plates, cups, napkins and cups)",
+              "Party Favors",
+              "3 Bouquet of Balloons",
+              "1 Table and 8 Chairs",
+              "Plastic Tablecloth "
+            ],
+            img: 'images/package5.jpg',
+            price: 2500,
+            note:[
+              "This package offers fun activities for children of all ages. This is a great addition to our DJ package or as an independent package.",
+            ]
+        },
+        {
+            title:'Easter Party Package',
+            description:'Activities include pretend play, contests, dress up, face painting, discovery, hands-on experiments, interactive cooking, entertainment, learning, & FUN',
+            highlights:[
+              "Up to 15 Kids",
+              "30 Minutes Consultation to discuss party details",
+              "Set up and break down of party decor",
+              "Party Time Line",
+              "Party Design",
+              "Candy buffet- 3-4 Candies",
+              "Choice of 2 Personalized Items",
+              "Theme Invites",
+              "Up to 3 Centerpieces",
+              "Place Settings (Disposable, plates, cups, napkins and cups)",
+              "Party Favors",
+              "3 Bouquet of Balloons",
+              "1 Table and 8 Chairs",
+              "Plastic Tablecloth "
+            ],
+            img: 'images/package7.jpg',
+            price: 3550,
+            note:[
+              "*Additonal kids are $12.00 each",
+              "*For additonal cost of $250.00 add a photographer and a 30 min photo shoot for the birthday kid prior to the party."
+            ]
+        },
+        {
+            title:'Outdoor Party Package',
+            description:'Activities include pretend play, contests, dress up, face painting, discovery, hands-on experiments, interactive cooking, entertainment, learning, & FUN',
+            highlights:[
+              "Up to 15 Kids",
+              "30 Minutes Consultation to discuss party details",
+              "Set up and break down of party decor",
+              "Party Time Line",
+              "Party Design",
+              "Candy buffet- 3-4 Candies",
+              "Choice of 2 Personalized Items",
+              "Theme Invites",
+              "Up to 3 Centerpieces",
+              "Place Settings (Disposable, plates, cups, napkins and cups)",
+              "Party Favors",
+              "3 Bouquet of Balloons",
+              "1 Table and 8 Chairs",
+              "Plastic Tablecloth "
+            ],
+            img: 'images/package9.jpg',
+            price: 6050,
+            note:[
+              "*Additonal kids are $12.00 each",
+              "*For additonal cost of $250.00 add a photographer and a 30 min photo shoot for the birthday kid prior to the party."
+            ]
         }
 ];
